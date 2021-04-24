0c6f2f309c28a8fad529a6f3d539383feb69ffc1
Jake Robertson <jake@faltro.com>
2021-04-24 13:47:28 -0400

    update manager callback structure

--- src/manager.js
+++ src/manager.js
@@ -1,9 +1,9 @@
-const { install } = require("./install");
+const install = require("./install");
 
 module.exports = (ipcMain, baseDir) => {
   ipcMain.on("hpm-install", (event, name, version) => {
-    install(name, version, baseDir, (err, installedPath) => {
-      event.sender.send(`hpm-installed-${name}`, err, installedPath);
+    install(name, version, baseDir, () => {
+      event.sender.send(`hpm-installed-${name}`);
     });
   });
 };
